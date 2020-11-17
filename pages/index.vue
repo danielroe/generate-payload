d60260daea4566b0352d918862e3d493c1c02064
Daniel Roe <daniel@roe.dev>
2020-11-17 22:18:40 +0000

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ test }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    test: 0,
+  }),
+  fetch() {
+    this.test = 24
+  },
+  fetchOnServer: false,
+}
+</script>
